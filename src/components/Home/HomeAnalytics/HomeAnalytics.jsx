c3fe8eb72e8b7887689c1a597d6865f938e22dcf
Manmeet-nap <manmeet@napworks.in>
2025-03-29 18:40:05 +0530

    ADD : Added about us component, made it responsive as well

--- src/components/Home/HomeAnalytics/HomeAnalytics.jsx
+++ src/components/Home/HomeAnalytics/HomeAnalytics.jsx
@@ -1,9 +1,68 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./HomeAnalytics.css";
+import happyImage from "../../../assets/happy-image.jpeg";
 
 function HomeAnalytics() {
-  return <div className="home-analytics">
+  const [isMobileView, setIsMobileView] = useState(window.innerWidth <= 840);
+
+  // Function to update the state based on window size
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobileView(window.innerWidth <= 840);
+    };
+
+    window.addEventListener("resize", handleResize);
     
-  </div>;
+    // Cleanup event listener on unmount
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <div className="home-analytics">
+      <div className="ha-content">
+        {isMobileView && (
+          <div className="hac-text">
+            <p>About Us</p>
+            <p>Caring for Smiles,</p>
+            <p>Big and Small</p>
+            <p>Your Family’s Trusted Dental Home!</p>
+          </div>
+        )}
+
+        <img src={happyImage} alt="happy" className="ha-happy-image" />
+
+        <div className="hac-right">
+          {!isMobileView && (
+            <>
+              <p>About Us</p>
+              <p>Caring for Smiles,</p>
+              <p>Big and Small</p>
+              <p>Your Family’s Trusted Dental Home!</p>
+            </>
+          )}
+
+          <div className="ha-analytics">
+            <div className="haa-container">
+              <p>20+</p>
+              <p>Years of Experience</p>
+            </div>
+            <div className="haa-container">
+              <p>750+</p>
+              <p>Happy Clients</p>
+            </div>
+            <div className="haa-container">
+              <p>98%</p>
+              <p>Satisfaction Rate</p>
+            </div>
+            <div className="haa-container">
+              <p>700+</p>
+              <p>Dental Implants</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default HomeAnalytics;
