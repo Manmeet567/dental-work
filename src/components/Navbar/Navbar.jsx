07cae334e539e8b38f1cbbe610f723d91d077065
Manmeet567 <manmeet567singh@gmail.com>
2025-04-20 17:25:37 +0530

    made navbar responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import logo from '../../assets/logo.png';
+import React, { useState } from "react";
+import logo from "../../assets/logo.png";
 import "./Navbar.css";
+import { TbMenuDeep } from "react-icons/tb";
+import { IoClose } from "react-icons/io5";
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <div className='navbar'>
+    <div className="navbar">
       <img src={logo} alt="logo" />
       <div className="menu-items">
         <a href="#">HOME</a>
@@ -14,8 +18,27 @@ function Navbar() {
         <a href="#">TESTIMONIALS</a>
         <a href="#">CONTACT US</a>
       </div>
+
+      {/* Responsive menu */}
+      <div className="responsive-menu">
+        <div className="rm-menu-icon" onClick={() => setMenuOpen(true)}>
+          <TbMenuDeep className="rmi-icon" />
+        </div>
+
+        <div className={`rm-menu ${menuOpen ? "open" : ""}`}>
+          <div className="rm-close-icon" onClick={() => setMenuOpen(false)}>
+            <IoClose />
+          </div>
+          <a href="#">HOME</a>
+          <a href="#">ABOUT US</a>
+          <a href="#">SERVICES</a>
+          <a href="#">FAQs</a>
+          <a href="#">TESTIMONIALS</a>
+          <a href="#">CONTACT US</a>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
